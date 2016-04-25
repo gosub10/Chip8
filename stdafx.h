cd70cc8ad5c26e704a187c4f07558d49dafc330e
Osman Öztürk <ozturk.osman@metu.edu.tr>
2016-04-26 02:30:46 +0300

    Libraries and types

--- stdafx.h
+++ stdafx.h
@@ -9,7 +9,15 @@
 
 #include <stdio.h>
 #include <tchar.h>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <random>
+#include <memory>
+#include <map>
+#include <list>
+#include <vector>
 
-
-
+#include "types.h"
 // TODO: reference additional headers your program requires here
